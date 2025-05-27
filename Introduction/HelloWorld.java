c3c70a9cbfdd1e78ebe09c799874504b7fe6aa07
Jack <jack.turner@vtsd.com>
2025-05-27 13:55:01 -0400

    h

---
+++ Introduction/HelloWorld.java
@@ -0,0 +1,15 @@
+package Introduction;
+
+// create class hello world
+public class HelloWorld {
+
+    //create main method
+    public static void main(String[] args) {
+        // output to the window hello world
+        System.out.println("Hello World");
+
+
+
+
+    }
+}
---
+++ Introduction/VariableIntro.java
@@ -0,0 +1,15 @@
+package Introduction;
+
+public class VariableIntro {
+
+    public static void main(String[] args) {}
+
+    // datatype name = value;
+    double salary = 10000.56;
+    int bonus = 250;
+
+    bonus = (int) salary;
+
+    System.out.println(salary);
+    System.out.println(bonus);
+}
