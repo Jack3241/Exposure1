37fbf548cc0ee876a78a8cdab252ea2bc7990cf5
Jack <jack.turner@vtsd.com>
2025-06-12 13:24:27 -0400

    final

--- Sieve/Lab11avst.java
+++ Sieve/Lab11avst.java
@@ -1,14 +1,13 @@
-package Sieve;
-// Lab11avst.java
-// The "Sieve of Eratosthenes" Program
-// This is the student, starting version of the Lab11a assignment.
-
+package sieve;
+    // Lab11avst.java
+    // The "Sieve of Eratosthenes" Program
+    // This is the student, starting version of the Lab11a assignment.
 
 import java.util.Scanner;
 
+
 public class Lab11avst {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // This main method needs additions for the 100 point version.
         Scanner input = new Scanner(System.in);
         final int MAX = 100;
@@ -17,36 +16,32 @@ public class Lab11avst {
         computePrimes(primes);
         displayPrimes(primes);
     }
-
-    public static void computePrimes(boolean primes[])
-    {
+    public static void computePrimes(boolean primes[]) {
         System.out.println("\nCOMPUTING PRIME NUMBERS");
-        // Assume everything is prime
-        for(int i = 2; i < primes.length; i++){
+        for (int i = 2; i < primes.length; i++) {
             primes[i] = true;
         }
 
-        // Student edits start here - 80 point version
 
+        for (int i = 2; i < primes.length; i++) {
+            if (primes[i]){
+                for (int t = i + i; t < primes.length; t = t+i) {
+                    primes[t] = false;
 
-        // Student edits end here - 80 point version
+                }
 
+            }
+
+        }
     }
-
-    public static void displayPrimes(boolean primes[])
-    {
-        System.out.println("\n\nPRIMES BETWEEN 1 AND "+ primes.length);
-
-        // Output all values of the list
-        for(int i = 2; i < primes.length; i++){
-            if(primes[i] == true) {
+    public static void displayPrimes(boolean primes[]) {
+        System.out.println("\n\nPRIMES BETWEEN 1 AND " + primes.length);
+        //output of all values of the list
+        for (int i = 2; i < primes.length; i++)
+            if (primes[i]) {
                 System.out.print(i + " ");
             }
-        }
 
         System.out.println();
-
     }
-
 }
-
